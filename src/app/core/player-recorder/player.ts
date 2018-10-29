32b90122d7e4148c4f612a5f7b6bd4cd2bf52ceb
Hibeekaey <hibeekaey@yahoo.com>
2018-10-29 16:42:06 +0100

    Text to speech complete

--- src/app/core/player-recorder/player.ts
+++ src/app/core/player-recorder/player.ts
@@ -1,37 +1,42 @@
-import { LoadingController } from '@ionic/angular';
-
 import { Media, MediaObject } from '@ionic-native/media/ngx';
 
 import { TextToSpeech, TextToSpeechService } from '../text-to-speech/text-to-speech.service';
-import { throwError } from 'rxjs';
-import { retryWhen, delayWhen} from 'rxjs/operators';
+import { interval, throwError, Subject } from 'rxjs';
+import { retryWhen, delayWhen, takeUntil } from 'rxjs/operators';
 
 export class Player {
   constructor(
-    public loadingController: LoadingController,
     public media: Media,
     public _textToSpeechService: TextToSpeechService
   ) {}
 
-  async presentLoading() {
-    const loading = await this.loadingController.create({
-      message: 'Hello',
-      duration: 2000
-    });
-    return await loading.present();
-  }
-
-  play(data): void {
+  play(data): Promise<number> {
     const audio: MediaObject = this.media.create(data);
+    const duration = interval(700);
+    const unsubscriber$: Subject<void> = new Subject<void>();
+    let previousPosition = 0;
     audio.play();
+    return new Promise(resolve => {
+      duration.pipe(takeUntil(unsubscriber$)).subscribe(
+        val => {
+          audio.getCurrentPosition().then(position => {
+            if (previousPosition === position) {
+              unsubscriber$.next();
+              resolve(val);
+            }
+            previousPosition = position;
+          });
+        }
+      );
+    });
   }
 
-  textToSpeech(text, code) {
+  textToSpeech(text, code): Promise<object> {
     const payload = { text: text, language: code };
     return new Promise((resolve, reject) => {
       this._textToSpeechService.process(payload).subscribe(
         (data: TextToSpeech) => {
-          resolve(data.data);
+          resolve(data);
         },
         error => {
           console.log('Text-to-Speech operation failed.');
